2ead994c9fab9e1819895de71f7e68abfd94b34f
omerxx <omerxx@gmail.com>
2018-07-28 02:09:07 +0300

    Fix sha

--- formula/fed.rb
+++ formula/fed.rb
@@ -4,7 +4,7 @@ class Fed < Formula
   desc "CLI to deploy Frontend modules"
   homepage "https://github.com/spotim/fed-cli"
   url "https://github.com/SpotIM/fed-cli/releases/download/v0.1.1/fed-cli-darwin-amd64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "caa9058ab48328baf8d2fdc95ab965636d61bc3e7f8eae07f8b21a52517ee938"
+  sha256 "5ec7bf41d6f779dacce00c37a649039640edbefc201f2b8e0e8dc5de7489ba08"
 
   # depends_on "cmake" => :build
 
