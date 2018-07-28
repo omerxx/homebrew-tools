1c7278fe9ec78d74c07b7faba4999058b1e30da5
omerxx <omerxx@gmail.com>
2018-07-28 19:14:48 +0300

    Removing swp file. encapsulating repo

--- formula/fed.rb
+++ formula/fed.rb
@@ -3,7 +3,7 @@ require "formula"
 class Fed < Formula
   desc "CLI to deploy Frontend modules"
   homepage "https://github.com/spotim/fed-cli"
-  url "https://github.com/SpotIM/fed-cli/releases/download/v0.1.1/fed-cli-darwin-amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/repo/fed-cli/releases/download/v0.1.1/fed-cli-darwin-amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "19f261ee6ba6d31ff5faa9125e90bf70c3081795d56377e8931b81c44fc23448"
   head "https://github.com/spotim/fed-cli.git"
 
