f7418f884562a3d446910455f7ab2cba77373405
omerxx <omerxx@gmail.com>
2018-07-30 01:54:27 +0300

    Bump tap to 0.1.2

--- formula/fed.rb
+++ formula/fed.rb
@@ -3,8 +3,8 @@ require "formula"
 class Fed < Formula
   desc "CLI to deploy Frontend modules"
   homepage "https://github.com/spotim/fed-cli"
-  url "https://github.com/spotim/fed-cli/releases/download/v0.1.1/fed-cli-darwin-amd64-0.1.1.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "d135b6185ccc181c1000c3e4c64521258ad14d7adaa173dc55a9a80cabf09f8b"
+  url "https://github.com/spotim/fed-cli/releases/download/v0.1.2/fed-cli-darwin-amd64-0.1.2.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "7c54317ce12e9bd07e0f65d52410421564523600f6f09aaacc3bf8644e2139fb"
   head "https://github.com/spotim/fed-cli.git"
 
   def install
