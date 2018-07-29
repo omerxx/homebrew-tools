b244367cd6e4a383cde90328ff235adcd038da13
omerxx <omerxx@gmail.com>
2018-07-30 01:41:15 +0300

    Updating tap

--- formula/fed.rb
+++ formula/fed.rb
@@ -3,8 +3,8 @@ require "formula"
 class Fed < Formula
   desc "CLI to deploy Frontend modules"
   homepage "https://github.com/spotim/fed-cli"
-  url "https://github.com/spotim/fed-cli/releases/download/v0.1.1/fed-cli-darwin-amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "161d65a72f102016f6a6cf06ba7f5812a7d9742121442e7a63c463046afe7bc7"
+  url "https://github.com/spotim/fed-cli/releases/download/v0.1.1/fed-cli-darwin-amd64-0.1.1.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "d135b6185ccc181c1000c3e4c64521258ad14d7adaa173dc55a9a80cabf09f8b"
   head "https://github.com/spotim/fed-cli.git"
 
   def install
